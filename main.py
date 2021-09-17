adc996cf62a36d3af875ee92847db2e9d2fac53a
mindym1 <76831690+mindy-m@users.noreply.github.com>
2021-09-17 03:00:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Declare a class called CrewCandidate with an __init__ that takes three parameters—name, mass, and scores. Note that scores will be an list of test results.
+
+# Follow the project instructions here: https://education.launchcode.org/lchs/chapters/classes/project.html
+
+
+
+# Part 4 - Use the methods to boost DustVac’s status to Backup or higher. How many tests will it take to reach Backup status? How many to reach Accepted? (Scores cannot exceed 100%).
+  
+def main():
+  # Create objects, call the class and methods, and add print statements here.
+
+  print("Hello, cleaning robots!")
+
+if __name__ == '__main__':
+  main()
